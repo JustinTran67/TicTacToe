02e66e7fae77465aae823f2361b50769621fbab6
Justin Tran <justintran@Justins-MacBook-Pro.local>
2025-10-02 17:11:02 -0700

    Functional buttons for the entire board that display X when clicked

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -4,28 +4,28 @@ import Gameboard from './Gameboard';
 export default function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null));
 
-    function handleClick() {
+    function handleClick(i) {
         const nextSquares = squares.slice();
-        nextSquares[0] = "X";
+        nextSquares[i] = "X";
         setSquares(nextSquares);
     }
     
     return (
         <div>
             <div className="board-row">
-                <Gameboard value={squares[0]} onSquareClick={handleClick} />
-                <Gameboard value={squares[1]}/>
-                <Gameboard value={squares[2]}/>
+                <Gameboard value={squares[0]} onSquareClick={() => handleClick(0)} />
+                <Gameboard value={squares[1]} onSquareClick={() => handleClick(1)} />
+                <Gameboard value={squares[2]} onSquareClick={() => handleClick(2)} />
             </div>
             <div className="board-row">
-                <Gameboard value={squares[3]}/>
-                <Gameboard value={squares[4]}/>
-                <Gameboard value={squares[5]}/>
+                <Gameboard value={squares[3]} onSquareClick={() => handleClick(3)} />
+                <Gameboard value={squares[4]} onSquareClick={() => handleClick(4)} />
+                <Gameboard value={squares[5]} onSquareClick={() => handleClick(5)} />
             </div>
             <div className="board-row">
-                <Gameboard value={squares[6]}/>
-                <Gameboard value={squares[7]}/>
-                <Gameboard value={squares[8]}/>
+                <Gameboard value={squares[6]} onSquareClick={() => handleClick(6)} />
+                <Gameboard value={squares[7]} onSquareClick={() => handleClick(7)} />
+                <Gameboard value={squares[8]} onSquareClick={() => handleClick(8)} />
             </div>
         </div>
     );
