68e45b642bf610e90c3424a366cfe2704c1fb337
Justin Tran <justintran@Justins-MacBook-Pro.local>
2025-10-02 18:21:31 -0700

    Created winning function but does not yet calculate tie

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -2,12 +2,53 @@ import React, {useState} from 'react';
 import Gameboard from './Gameboard';
 
 export default function Board() {
+    const[xTurn, setXTurn] = useState(true);
     const [squares, setSquares] = useState(Array(9).fill(null));
 
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+        status = "Winner: " + winner;
+    } else {
+        if (xTurn) {
+            status = "Turn: X";
+        } else {
+            status = "Turn: O";
+        }
+    }
+
     function handleClick(i) {
+        if (squares[i] || calculateWinner(squares)) {
+            return;
+        }
         const nextSquares = squares.slice();
-        nextSquares[i] = "X";
+        if (xTurn) {
+            nextSquares[i] = "X";
+        } else {
+            nextSquares[i] = "O";
+        }
         setSquares(nextSquares);
+        setXTurn(!xTurn);
+    }
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for (let i = 0; i < lines.length; i++) {
+            const[a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null;
     }
     
     return (
@@ -26,6 +67,9 @@ export default function Board() {
                 <Gameboard value={squares[6]} onSquareClick={() => handleClick(6)} />
                 <Gameboard value={squares[7]} onSquareClick={() => handleClick(7)} />
                 <Gameboard value={squares[8]} onSquareClick={() => handleClick(8)} />
+            </div>
+            <div>
+                <p>{status}</p>
             </div>
         </div>
     );
